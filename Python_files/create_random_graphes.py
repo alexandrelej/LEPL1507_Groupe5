7663477a1ef532d2f0754c57fda4c822cc1eac29
flthyrion <florent.thyrion@student.uclouvain.be>
2025-02-11 10:57:16 +0100

    Start

--- Python_files/create_random_graphes.py
+++ Python_files/create_random_graphes.py
@@ -67,21 +67,22 @@ def create_random_subgraph(G, n, m):
     return subgraph
 
 
-# Chemins vers les fichiers CSV
-airports_csv = "../basic_datasets/airports.csv"
-routes_csv = "../basic_datasets/pre_existing_routes.csv"
+if __name__ == "__main__":
+    # Chemins vers les fichiers CSV
+    airports_csv = "../basic_datasets/airports.csv"
+    routes_csv = "../basic_datasets/pre_existing_routes.csv"
 
-# Créer le graphe complet
-airport_graph = create_airport_graph(airports_csv, routes_csv)
+    # Créer le graphe complet
+    airport_graph = create_airport_graph(airports_csv, routes_csv)
 
-# Générer un sous-graphe aléatoire
-n = int(input("Nombre de nœuds dans le sous-graphe = "))
-m = int(input("Nombre d'arêtes dans le sous-graphe = "))
-random_subgraph = create_random_subgraph(airport_graph, n, m)
+    # Générer un sous-graphe aléatoire
+    n = int(input("Nombre de nœuds dans le sous-graphe = "))
+    m = int(input("Nombre d'arêtes dans le sous-graphe = "))
+    random_subgraph = create_random_subgraph(airport_graph, n, m)
 
-# Afficher des informations sur le sous-graphe
-print("Sous-graphe aléatoire :")
-print("Nombre de nœuds :", random_subgraph.number_of_nodes())
-print("Nombre d'arêtes :", random_subgraph.number_of_edges())
-print("Liste des nœuds :", list(random_subgraph.nodes(data=True)))
-print("Liste des arêtes :", list(random_subgraph.edges(data=True)))
+    # Afficher des informations sur le sous-graphe
+    print("Sous-graphe aléatoire :")
+    print("Nombre de nœuds :", random_subgraph.number_of_nodes())
+    print("Nombre d'arêtes :", random_subgraph.number_of_edges())
+    print("Liste des nœuds :", list(random_subgraph.nodes(data=True)))
+    print("Liste des arêtes :", list(random_subgraph.edges(data=True)))
